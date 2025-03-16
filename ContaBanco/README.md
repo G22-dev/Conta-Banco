b3ce01ad28f6bf558c539dc5020c60beaacb2550
Guilherme Pinheiro <pinheirog972@gmail.com>
2025-03-16 18:21:35 -0300

    Ajustando Readme

--- ContaBanco/README.md
+++ ContaBanco/README.md
@@ -1,7 +1,28 @@
-# Conta Banco
+# ContaBanco - Projeto Java
+
+Porjeto Desenvolvido por Guilherme Pinheiro.
+
+## Tecnologias Utilizadas
+
+* Java (JDK 11 ou Superior)
+
+## Descrição
+
+Projeto Desenvolvido para simular uma sistema bancario simples em java. Ele solicita as informações ao usuario sobre sua conta bancaria(agencia e numero da conta) e exibe uma mensagem personalizada com o saldo da conta e os dados fornecidos.
+
+## Exemplo de Uso
+
+```
+Por favor, digite o número da Agência:
+
+160-0
+
+Por favor, digite o número da Conta:
+
+12
+```
+Saida de dados do Programa
+
+<img src=img\TerminalDados.png>
 
 
-## Informações usuarios
-
-
-### Saidas de dados
--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -4,21 +4,21 @@ public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         // Dados da conta
-        String Nome = "Antonio Guilherme";
-        double Saldo = 1000.0;
+        String nome = "Antonio Guilherme";
+        double saldo = 1000.0;
 
         // Solicitando informações da conta
         System.out.println("Por favor, digite o número da Agência: ");
-        String agencia = scanner.next();
+        String agencia = scanner.nextLine();
 
         System.out.println("Por favor, digite o número da Conta: ");
-        int Conta = scanner.nextInt();
+        int conta = scanner.nextInt();
 
         scanner.close();
 
         // Exibindo informações da conta
-        System.out.println("Olá " + Nome + " , obrigado por criar uma conta em nosso banco!");
-        System.out.println("Sua agência é "+ agencia +" , conta "+ Conta);
-        System.out.println("Seu saldo atual é de R$ " + Saldo);
+        System.out.println("Olá " + nome + " , obrigado por criar uma conta em nosso banco!");
+        System.out.println("Sua agência é "+ agencia +" , conta "+ conta);
+        System.out.println("Seu saldo atual é de R$ " + saldo);
     }
 }
