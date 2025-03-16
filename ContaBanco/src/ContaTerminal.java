6d64fd457ad7bc50a789faf34bb422194b845e69
Guilherme Pinheiro <pinheirog972@gmail.com>
2025-03-15 22:29:14 -0300

    Iniciando a aplicação

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
