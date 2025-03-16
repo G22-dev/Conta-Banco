ef4a12f08ede407b97398f22ca92f4e0805f10fd
Guilherme Pinheiro <pinheirog972@gmail.com>
2025-03-15 23:27:14 -0300

    Finalizando Aplicação

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -3,6 +3,22 @@ import java.util.Locale;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        // Dados da conta
+        String Nome = "Antonio Guilherme";
+        double Saldo = 1000.0;
+
+        // Solicitando informações da conta
+        System.out.println("Por favor, digite o número da Agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o número da Conta: ");
+        int Conta = scanner.nextInt();
+
+        // Exibindo informações da conta
+        System.out.println("Olá " + Nome + " , obrigado por criar uma conta em nosso banco!");
+        System.out.println("Sua agência é "+ agencia +" , conta "+ Conta);
+        System.out.println("Seu saldo atual é de R$ " + Saldo);
     }
 }
